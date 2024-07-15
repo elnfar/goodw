a7108da87eb0f3b4a8333157fcc13c66a0423403
elnfar <farzaliyevelnur00@gmail.com>
2024-07-15 22:07:10 +0400

    added error.tsx and project.length-null

--- src/app/(dashboard)/projects/page.tsx
+++ src/app/(dashboard)/projects/page.tsx
@@ -10,26 +10,29 @@ import ButtonClient from '@/components/global/ButtonClient'
 export default async function page() {
 
 
-  const user = await auth()
+  const session = await auth()
 
   
   const projects = await prisma.project.findMany({
     where: {
       //@ts-ignore
-      tenantId: user?.user?.tenant.id
+      tenantId: session?.user?.tenantId
     }
   })
   
 
+
+
+
+  if(projects.length === 0) return (
+     <p className='p-4 text-white text-2xl'>No project found</p>
+  )
 
   
 
 
   return (
     <div>
-
-      {projects.length === 0 ? <p className='p-4 text-white text-2xl'>No project found</p> : null}
-
       <ButtonClient title='Create'/>
       <div className='py-10'>
         {projects.map((project) => <ProjectSingle key={project.id} project={project}/>)}
