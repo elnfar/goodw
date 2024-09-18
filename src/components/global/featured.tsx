c41b72242845c21dfe1b08012778933374c5d230
elnfar <farzaliyevelnur00@gmail.com>
2024-09-18 23:33:28 +0300

    tags and workspace removed

--- src/components/global/featured.tsx
+++ src/components/global/featured.tsx
@@ -1,4 +1,5 @@
 import { ArrowBigDownDash, Cloud, CogIcon, FingerprintIcon, Lock, Server } from "lucide-react"
+import Image from "next/image"
 
   
   const features = [
@@ -39,15 +40,15 @@ import { ArrowBigDownDash, Cloud, CogIcon, FingerprintIcon, Lock, Server } from 
       <div className="bg-black py-24 sm:py-12">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
-            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-5xl">What is Aiiz?</p>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-5xl">What is Siiz?</p>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Aiiz is unopinionated, one-stop project management software built for teams that obsess over unlocking customer value in all they do.
+              Siizz is unopinionated, one-stop project management software built for teams that obsess over unlocking customer value in all they do.
             </p>
           </div>
         </div>
         <div className="relative overflow-hidden pt-16">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <img
+            <Image
               alt="App screenshot"
               src="/z.png"
               width={2432}
@@ -58,19 +59,6 @@ import { ArrowBigDownDash, Cloud, CogIcon, FingerprintIcon, Lock, Server } from 
               <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white" />
             </div>
           </div>
-        </div>
-        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8 ">
-          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative pl-9">
-                <dt className="inline font-semibold text-gray-400">
-                  <feature.icon aria-hidden="true" className="absolute left-1 top-1 h-5 w-5 text-white" />
-                  {feature.name}
-                </dt>{' '}
-                <dd className="inline">{feature.description}</dd>
-              </div>
-            ))}
-          </dl>
         </div>
       </div>
     )
